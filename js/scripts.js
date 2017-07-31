8ab34c014111c69796de78527ae4812aa3509470
85bogdan <daniel.t.bogucki@gamil.com>
2017-07-31 09:32:20 +0200

    add warranty

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
+
 function Phone(brand, price, color, size) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
 	this.size = size;
 }
+
+Phone.prototype.getWarrantyCost=function() {
+	var warrantyCost = (this.price * 0.1);
+	console.log(warrantyCost);
+	}
+
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", size is " + this.size + " and the price is " + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", size is " + this.size + " and the price is " + this.price + " and the warranty cost is" + this.warrantyCost + ".");
 }
 
 var iPhone6S = new Phone("Apple", 2250, "silver", '5,2"');
 var samsungGalaxyS6 = new Phone("Samsung", 1980, "white", '5,5"');
 var onePlusOne = new Phone("One Plus", 1730, "black", '5,2"');
 
+
+iPhone6S.getWarrantyCost();
+samsungGalaxyS6.getWarrantyCost();
+onePlusOne.getWarrantyCost();
 iPhone6S.printInfo();
 samsungGalaxyS6.printInfo();
 onePlusOne.printInfo();
