4ab3bbfed2e86c132b1684a559dc0379fb28b1d9
85bogdan <daniel.t.bogucki@gamil.com>
2017-07-31 09:48:29 +0200

    add id

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,31 @@
 $(function(){
 	console.log('DOM loaded - you can have fun');
-});
 
-$("span:even").css('color', 'red');
 
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
+	var spans = $('span')
+		
+	spans.each(function(element){
+		var ignore = ['second', 'third'];
+		var id = spans.attr('id');
+		if (ignore.indexOf(id) == -1){
+			$(element).css('color', 'red');
+		}
+	});
 
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
 
-});
 
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+	var paragraphs = $('p');
+	paragraphs.each(function(index, element) {
+
+    	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    	$(element).append(button)
+
+	});
+
+	$("button").click(function(){
+		alert($(this).attr("data-tmp"));
+
+
+	});
+
 });
